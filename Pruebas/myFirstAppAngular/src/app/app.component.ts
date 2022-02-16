dab2a0277acf3ea81f19d482e6805b186f7f5ab2
Sanss <santi-2000-12@hotmail.com>
2022-02-15 23:50:48 -0500

    New prueba Angular

---
+++ Pruebas/myFirstAppAngular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myFirstAppAngular';
+  saludo = 'Esto es un saludo desde el componente root'
+}
