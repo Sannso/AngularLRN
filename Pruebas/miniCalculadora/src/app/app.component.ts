ab207b8030af27bca3b8e6796a9ed5ddfea3d48b
Sanss <santi-2000-12@hotmail.com>
2022-03-04 00:32:13 -0500

    Class 14 DONE

---
+++ Pruebas/miniCalculadora/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mini Calculadora';
+  
+  numero1:number = 0
+  numero2:number = 0
+  resultado:number = 0
+
+  setOperacion(event:Event){
+    
+    if((<HTMLInputElement>event.target).value == "Sumar"){
+      this.resultado = (this.numero1 + this.numero2)
+    }else{
+      this.resultado = (this.numero1 - this.numero2)
+    }
+  }
+
+}
